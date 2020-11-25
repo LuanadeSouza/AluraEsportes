ebbc2a8e38a762e38b2e9234f4ad266f77bcb908
Luana Souza <luana.souza@ifood.com.br>
2020-11-25 18:07:38 -0300

    configurando navigation

--- app/src/main/java/br/com/luanadev/esportesapplication/database/dao/PagamentoDAO.kt
+++ app/src/main/java/br/com/luanadev/esportesapplication/database/dao/PagamentoDAO.kt
@@ -2,7 +2,7 @@ package br.com.luanadev.esportesapplication.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import br.com.alura.aluraesporte.model.Pagamento
+import br.com.luanadev.esportesapplication.model.Pagamento
 
 @Dao
 interface PagamentoDAO {
